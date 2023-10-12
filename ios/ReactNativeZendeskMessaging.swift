f54fb21e64ce55e78c7e328080a3e832a7ea95a1
Daniel Israel <danny@gooseinsurance.com>
2023-10-12 11:36:10 -0700

    Fixed update push notification token for iOS.

--- ios/ReactNativeZendeskMessaging.swift
+++ ios/ReactNativeZendeskMessaging.swift
@@ -60,16 +60,28 @@ class ZendeskMessaging: NSObject {
     }
   }
 
-  @objc
-  func updatePushNotificationToken(_ deviceToken:String,
-    resolver resolve: @escaping RCTPromiseResolveBlock,
-    rejecter reject: @escaping RCTPromiseRejectBlock
-  ){
-    do{
-      try PushNotifications.updatePushNotificationToken(Data(deviceToken.utf8))
-      resolve("success");
-    } catch {
-      reject("error","\(error)",nil)
+    @objc
+    func updatePushNotificationToken(_ deviceToken:String,
+      resolver resolve: @escaping RCTPromiseResolveBlock,
+      rejecter reject: @escaping RCTPromiseRejectBlock
+    ){
+        let len = deviceToken.count / 2
+        var data = Data(capacity: len)
+        var i = deviceToken.startIndex
+        for _ in 0..<len {
+          let j = deviceToken.index(i, offsetBy: 2)
+          let bytes = deviceToken[i..<j]
+          if var num = UInt8(bytes, radix: 16) {
+              data.append(&num, count: 1)
+          }
+          i = j
+        }
+      do{
+        try
+          PushNotifications.updatePushNotificationToken(data)
+          resolve("success");
+      } catch {
+          reject("error","\(error)",nil)
+      }
     }
-  }
 }
